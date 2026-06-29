3a80e8e37fdbf83a9b71c287f69647cc580eca03
santhoshsathish920-beep <santhoshsathish920@gmail.com>
2026-06-29 10:45:57 +0530

    Fix ALLOWED_HOSTS for Vercel

--- glrms/settings.py
+++ glrms/settings.py
@@ -18,7 +18,14 @@ SECRET_KEY = config('SECRET_KEY', default='your-django-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'glrms.vercel.app',
+    '.vercel.app',
+]
 
 # Application definition
 INSTALLED_APPS = [
